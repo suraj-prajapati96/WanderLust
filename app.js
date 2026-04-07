b5c0e93af5343c2960c14ad4c0ae1bbd65850634
Suraj Prajapati <sp1201582@gmail.com>
2026-04-07 21:41:06 +0530

    second commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate');
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/expressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
 main().then(()=>{
@@ -29,6 +30,18 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.get("/",(req,res)=>{
     res.send("Hello this is root");
 })
+
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }else{
+        next();
+    }
+}
+
+
 //Route Index
 app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings = await Listing.find({});
@@ -39,10 +52,7 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new");
 })
 //Create Route
-app.post("/listings",wrapAsync(async(req,res,next)=>{
-   if(!req.body.listing){
-        throw new ExpressError(502, "Send valid data for listing");
-   }
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.status(200).redirect("/listings");
@@ -64,7 +74,7 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
 }));
 
 //Update List
-app.put("/listings/:id",wrapAsync(async(req,res)=>{
+app.put("/listings/:id",validateListing, wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -93,13 +103,13 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
 
 //error handling
 
-app.use((req,res,next)=>{
+app.all("*path",(req,res,next)=>{
     next(new ExpressError(404,"Page Not Found!"));
 });
 
 app.use((err,req,res,next)=>{
-    let {statusCode, message} = err;
-    res.status(statusCode).send("Something is wrong!");
+    let {statusCode = 500, message = "Something went wrong"} = err;
+    res.status(statusCode).render("error.ejs",{message});
 });
 app.listen(8000,()=>{
     console.log("App is listening on port 8000");
